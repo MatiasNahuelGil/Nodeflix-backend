3d6408a4f8c91b93598b552976baf3249f793115
MatiasDev <gilmatiasnahuel619@gmail.com>
2024-07-03 19:55:08 -0300

    Add files via upload

---
+++ genero.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM nodeflix.genero
+LIMIT 0, 1000
+
+-- Date: 2024-07-03 19:53
+*/
+INSERT INTO `` (`id`,`nombre`) VALUES (1,'Ciencia-Ficcion');
+INSERT INTO `` (`id`,`nombre`) VALUES (2,'Terror');
+INSERT INTO `` (`id`,`nombre`) VALUES (3,'Accion');
+INSERT INTO `` (`id`,`nombre`) VALUES (4,'Romance');
