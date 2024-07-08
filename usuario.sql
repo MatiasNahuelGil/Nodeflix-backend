6cec2fc130208b35622df5c5f88f324e358decd2
MatiasDev <gilmatiasnahuel619@gmail.com>
2024-07-08 18:32:22 -0300

    Add files via upload

---
+++ usuario.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM nodeflix.usuario
+LIMIT 0, 1000
+
+-- Date: 2024-07-08 18:32
+*/
+INSERT INTO `` (`id`,`user`,`password`) VALUES (1,'Jaime','jaime1234');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (2,'jose','$2a$08$qsXCym211fmy3cup8lF/MudNeM.M/Q3moP/KbMczIhEE.VU3v6ObO');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (3,'jose','$2a$08$rDoN.C1dNcH.0e0rex3VYeOgI7oYHUFbrnPhzE.1ttyrrRJJttfMy');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (4,'marcos','$2a$08$.vAkzpNYmPki31VJG1GPFOS2W/SMod.d/gazrNH10HjaSPV5jwR7e');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (5,'julieta','$2a$08$KsRX3Xo8br3WrhIZHD8bWOjAK/4KO8kz/5SlqILrZyMY3EHZpMGuK');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (6,'Roberto','$2a$08$lDBvCs7sDyPMi/1O6E1iuelmNNjGjFvTE.iOBQasA1/cBuTNegJse');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (7,'adrian','$2a$08$6JS5m6NvbwbidoRJnfAbj.BMu8yE.GSaZy5GOQz/1i9FjzNXK5R2e');
+INSERT INTO `` (`id`,`user`,`password`) VALUES (8,'Ramiro','$2a$08$KuyKTWcMRPDBSlT6sBR37uSrtUruSzLf2V6wFJ0LiwNdQFpmkvhUu');
